4531bbf5aa3ed6069f444f925e62b062ffabcc79
fuyu <88916226+fuyu28@users.noreply.github.com>
2025-07-28 16:38:27 +0900

    feat:importのルールを追加

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,7 @@ import eslintConfigPrettier from "@electron-toolkit/eslint-config-prettier"
 import eslintPluginReact from "eslint-plugin-react"
 import eslintPluginReactHooks from "eslint-plugin-react-hooks"
 import eslintPluginReactRefresh from "eslint-plugin-react-refresh"
+import eslintPluginImport from "eslint-plugin-import"
 
 export default tseslint.config(
   { ignores: ["**/node_modules", "**/dist", "**/out"] },
@@ -20,11 +21,36 @@ export default tseslint.config(
     files: ["**/*.{ts,tsx}"],
     plugins: {
       "react-hooks": eslintPluginReactHooks,
-      "react-refresh": eslintPluginReactRefresh
+      "react-refresh": eslintPluginReactRefresh,
+      import: eslintPluginImport
     },
     rules: {
       ...eslintPluginReactHooks.configs.recommended.rules,
-      ...eslintPluginReactRefresh.configs.vite.rules
+      ...eslintPluginReactRefresh.configs.vite.rules,
+      "import/order": [
+        "warn",
+        {
+          groups: [
+            "builtin", // Node.jsビルトインモジュール
+            "external", // 外部ライブラリ（reactなど）
+            "internal", // 自作モジュール（@やエイリアスなど）
+            ["parent", "sibling", "index", "type"] // 相対パス
+          ],
+          pathGroups: [
+            {
+              pattern: "./ipcHandlers/**",
+              group: "internal",
+              position: "after"
+            }
+          ],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: true
+          },
+          "newlines-between": "always"
+        }
+      ],
+      "@typescript-eslint/consistent-type-imports": ["error", { prefer: "type-imports" }]
     }
   },
   eslintConfigPrettier
